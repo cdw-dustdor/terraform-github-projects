f3aae13011252c19c1c239309b8ebe900a673a7f
Dustin Dortch <dustin.dortch@cdw.com>
2024-03-07 11:24:15 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.42.0.0/24"
+}
